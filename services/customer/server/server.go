7d934e01c5f424cbabcb3761ba9e1f037f5684f3
Aidos Galimzhan <85453907+aidosgal@users.noreply.github.com>
2025-10-21 02:47:32 +0500

    Feat: Add logs to server(#6)
    * Feat: Add README.md
    
    * Feat: Add logs to server

--- services/customer/server/server.go
+++ services/customer/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/aidosgal/transline-test/services/customer/entity"
 	"github.com/aidosgal/transline-test/services/customer/usecase"
@@ -10,29 +11,47 @@ import (
 
 type server struct {
 	pb.UnimplementedCustomerServer
+	log     *slog.Logger
 	usecase usecase.Usecase
 }
 
-func New (usecase usecase.Usecase) *server {
+func New(log *slog.Logger, usecase usecase.Usecase) *server {
 	return &server{
+		log:     log.With("layer", "server"),
 		usecase: usecase,
 	}
 }
 
 func (s *server) UpsertCustomer(ctx context.Context, req *pb.UpsertCustomerRequest) (*pb.CustomerResponse, error) {
+	log := s.log.With("method", "UpsertCustomer")
+
+	log.Info("received upsert customer request", slog.String("idn", req.GetIdn()))
+
 	resp, err := s.usecase.UpsertCustomer(ctx, req.GetIdn())
 	if err != nil {
+		log.Error("failed to upsert customer", slog.String("error", err.Error()))
 		return nil, err
 	}
-	
+
+	log.Info("customer upserted successfully",
+		slog.String("customer_id", resp.ID),
+		slog.String("idn", resp.IDN))
 	return entity.MakeCustomerEntityToPb(resp), nil
 }
 
 func (s *server) GetCustomer(ctx context.Context, req *pb.GetCustomerRequest) (*pb.CustomerResponse, error) {
+	log := s.log.With("method", "GetCustomer")
+
+	log.Info("received get customer request", slog.String("idn", req.Idn))
+
 	resp, err := s.usecase.GetCustomer(ctx, req.Idn)
 	if err != nil {
+		log.Error("failed to get customer", slog.String("error", err.Error()))
 		return nil, err
 	}
-	
+
+	log.Info("customer retrieved successfully",
+		slog.String("customer_id", resp.ID),
+		slog.String("idn", resp.IDN))
 	return entity.MakeCustomerEntityToPb(resp), nil
 }
