2c35b9f9228589e42f199feced13935178b3fb38
A.Galimzhan <a.galimzhan@redmadrobot.kz>
2025-10-21 02:52:50 +0500

    Feat: Make logs more informative in usecase

--- services/customer/usecase/usecase.go
+++ services/customer/usecase/usecase.go
@@ -27,29 +27,36 @@ func New(log *slog.Logger, storage storage.Storage) Usecase {
 }
 
 func (u *usecase) GetCustomer(ctx context.Context, idn string) (*entity.Customer, error) {
-	log := u.log.With("method", "GetCustomer")
+	log := u.log.With("method", "GetCustomer", "idn", idn)
 	
-	log.Debug("started storage.GetCustomerByIDN", slog.String("idn", idn))
+	log.Info("getting customer from storage")
 	
 	customer, err := u.storage.GetCustomerByIDN(ctx, idn)
 	if err != nil {
-		log.Error("failed to storage.GetCustomerByIDN", slog.String("error", err.Error()))
+		log.Error("failed to get customer from storage", slog.String("error", err.Error()))
 		return nil, fmt.Errorf("failed to storage.GetCustomerByIDN: %w", err)
 	}
 	
+	log.Info("customer found successfully", 
+		slog.String("customer_id", customer.ID),
+		slog.String("created_at", customer.CreatedAt.String()))
 	return customer, nil
 }
 
 func (u *usecase) UpsertCustomer(ctx context.Context, idn string) (*entity.Customer, error) {
-	log := u.log.With("method", "UpsertCustomer")
+	log := u.log.With("method", "UpsertCustomer", "idn", idn)
 	
-	log.Debug("started storage.UpsertCustomer", slog.String("idn", idn))
+	log.Info("upserting customer in storage")
 	
 	customer, err := u.storage.UpsertCustomer(ctx, idn)
 	if err != nil {
-		log.Error("failed to storage.UpsertCustomer", slog.String("error", err.Error()))
+		log.Error("failed to upsert customer in storage", slog.String("error", err.Error()))
 		return nil, fmt.Errorf("failed to storage.UpsertCustomer: %w", err)
 	}
 	
+	log.Info("customer upserted successfully",
+		slog.String("customer_id", customer.ID),
+		slog.String("idn", customer.IDN),
+		slog.String("created_at", customer.CreatedAt.String()))
 	return customer, nil
 }
